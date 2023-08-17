bc0d9d99da119845363fbc874f72a6fa4be7bee6
Sam Sebastian <sebastiansam55@gmail.com>
2023-08-17 07:48:08 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+import xml.etree.ElementTree as ET
+from pymongo import MongoClient
+import argparse
+import uuid
+import datetime
+
+def try_convert(value):
+    try:
+        return int(value)
+    except ValueError:
+        try:
+            return float(value)
+        except ValueError:
+            try:
+                return datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f%z')
+            except ValueError:
+                return value
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(
+            prog='xmltomongodb',
+            description='script that converts xml to mongodb documents'
+    )
+    parser.add_argument('importfile', help="XML File to be imported")
+    parser.add_argument('-m','--mongodb', help="MongoDB connection string", default="mongodb://localhost")
+    parser.add_argument('database', help="Database to import to", default='sqltrace')
+    parser.add_argument('destcollection', help="Collection to insert to", default='trace')
+
+
+    args = parser.parse_args()
+    CONNECTION_STRING = args.mongodb
+    client = MongoClient(CONNECTION_STRING)
+
+    db = client[args.database]
+    colname=args.destcollection
+    collection = db[colname]
+
+    count = collection.count_documents({})
+    if count==0:
+        pass
+    else:
+        collectionname = 'trace'+str(uuid.uuid4())
+        collection = db[collectionname]
+        print(collectionname)
+        pass
+
+    tree = ET.parse('import.xml')
+    root = tree.getroot()
+    #print(root)
+    for child in root:
+        if 'Events' in child.tag:
+            count = 0
+            for event in child:
+                eventname = event.attrib['name']
+                eventid = event.attrib['id']
+                mongodoc = {"event":eventname, "_id":count}
+                for column in event:
+                    try:
+                        pass
+                    except:
+                        pass
+                    mongodoc[column.attrib['name']] = try_convert(column.text)
+                #print(mongodoc)
+                collection.insert_one(mongodoc)
+                count+=1
